aae00513c1f2af6139ccefe996df8c4b9db82cbc
Ayobanji Ayodele <ay.ayobanji@gmail.com>
2024-06-22 18:55:56 +0100

    unit testing

--- backend/dashboard/urls.py
+++ backend/dashboard/urls.py
@@ -4,5 +4,7 @@ from .import views
 urlpatterns = [
 path("tasks/", views.TaskListCreate.as_view(), name="task-list"), 
 path("tasks/delete/<int:pk>/", views.TaskDelete.as_view(), name="delete-task"),
-path('tasks/update/<int:pk>/', views.TaskUpdateView.as_view(), name='update-task'),
+path('tasks/update/<int:pk>/', views.TaskUpdate.as_view(), name='update-task'),
+path('tasks/completed/', views.CompletedTasks.as_view(), name="completed-task"),
+path('tasks/overdue/', views.OverdueTasks.as_view(), name="overdue-task"),
 ]
--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -4,9 +4,9 @@ from rest_framework import generics
 from .serializers import UserSerializer, TaskSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Task
+from datetime import date
 
 
-# Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -18,7 +18,14 @@ class TaskListCreate(generics.ListCreateAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        return Task.objects.filter(assigned_to=user)
+        # Base queryset for all tasks assigned to the user
+        queryset = Task.objects.filter(assigned_to=user)
+
+        # Filter by status based on the request parameter 'status'
+        status = self.request.query_params.get('status')
+        if status:
+            queryset = queryset.filter(status=status)
+        return queryset
     
     def perform_create(self, serializer):
         if serializer.is_valid():
@@ -34,7 +41,7 @@ class TaskDelete(generics.DestroyAPIView):
         user = self.request.user
         return Task.objects.filter(assigned_to=user)
 
-class TaskUpdateView(generics.UpdateAPIView):
+class TaskUpdate(generics.UpdateAPIView):
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
 
@@ -47,3 +54,26 @@ class TaskUpdateView(generics.UpdateAPIView):
             serializer.save()
         else:
             print(serializer.errors)
+
+
+class CompletedTasks(TaskListCreate):
+#   View to retrieve completed tasks for the authenticated user.
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()  # Call base get_queryset for user filtering
+        return queryset.filter(status='Completed')  # Override to filter for completed tasks
+
+class OverdueTasks(TaskListCreate):
+
+    # View to retrieve overdue tasks for the authenticated user.
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()  # Call base get_queryset for user filtering
+        # Implement logic to filter overdue tasks based on your definition of overdue
+        # This example assumes a 'due_date' field on the Task model
+        today = date.today()
+        return queryset.filter(status='In Progress', due_date__lt=today).exclude(due_date=None)
